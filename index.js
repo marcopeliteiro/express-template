dbf5c39e9cc713a9f75c5cb4f1e010cebb9585ba
Marco Peliteiro <83987191+marcopeliteiro@users.noreply.github.com>
2026-05-21 21:22:35 +0100

    fix: add ejs to index

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import ExampleRouter from "./routes/ExampleRouter.js";
+import ejs from "ejs";
+import path from "path";
 
 const app = express();
 const PORT = 3000;
+app.set("view engine", "ejs");
+app.set("views", path.join(import.meta.dirname, "views"));
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/router", ExampleRouter);
 
 app.get("/", (req, res) => {
   res.send({ message: "Backend rodando com TypeScript e Imports!" });
+});
+
+app.get("/view", (req, res) => {
+  res.render("index");
 });
 
 app.listen(PORT, () => {
