30b04cda46d2e44dee307ce09bb31a05bacb440f
Marco Peliteiro <83987191+marcopeliteiro@users.noreply.github.com>
2026-05-21 20:57:23 +0100

    add examplerouter to index

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from "express";
 import * as dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
+import ExampleRouter from "./routes/ExampleRouter.js";
 
 const app = express();
 const PORT = 3000;
 app.use(cors());
 
 app.use(express.json());
+
+app.use("/router", ExampleRouter);
 
 app.get("/", (req, res) => {
   res.send({ message: "Backend rodando com TypeScript e Imports!" });
